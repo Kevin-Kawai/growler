8d3537480154a14c46aa8e7daa99bc1f149e847f
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-06-18 22:51:05 +0900

    dropdown multiple choices

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import Vue from 'vue/dist/vue.js'
 let growler = new Vue({
   el: '#growler',
   data: {
-    selectedSearchIndex: 'beers'
+    selectedSearchIndexes: [
+      'beers',
+      'pubs'
+    ]
   }
 });
