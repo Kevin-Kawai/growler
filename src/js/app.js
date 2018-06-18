cc7ff5ba17b91bea6844e579df394284d86219eb
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-06-18 22:40:09 +0900

    Binding checkboxes

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import Vue from 'vue/dist/vue.js'
 let growler = new Vue({
   el: '#growler',
   data: {
-    appName: 'Growler',
-    query: ''
+    isPowerSyntaxEnabled: false,
+    searchIndexs: []
   }
 });
