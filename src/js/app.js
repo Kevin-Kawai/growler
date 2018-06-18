3ecf96013363eeac379566771f314f3159903262
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-06-18 21:53:59 +0900

    model directive input text field

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import Vue from 'vue/dist/vue.js'
 let growler = new Vue({
   el: '#growler',
   data: {
-    appName: "<h1>hello world</h1>",
-    color: '#FF6A00'
+    appName: 'Growler',
+    query: ''
   }
 });
