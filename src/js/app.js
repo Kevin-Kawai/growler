c78bc827c62ae14a97a366905457ac167e6a93e6
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-06-19 06:55:04 +0900

    event parameter search with token

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,12 @@ import Vue from 'vue/dist/vue.js'
 let growler = new Vue({
   el: '#growler',
   data: {
-    selectedSearchIndexes: [
-      'beers',
-      'pubs'
-    ]
+    query: ''
+  },
+  methods: {
+    executeSearch:function(t, e) {
+      var msg = 'Token: ' + ' Query: ' + this.query + ' Button: ' + event.target.innerText;
+    alert(msg);
+    }
   }
 });
