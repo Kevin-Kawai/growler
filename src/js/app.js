92277a8e9cbb9349c8626a89968cb834a7196dd6
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-06-19 23:09:03 +0900

    conditionally rendering the view

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,13 @@ import Vue from 'vue/dist/vue.js'
 let growler = new Vue({
   el: '#growler',
   data: {
-    pages: [
-      { number: 1, url: '?page=1', sections: [ 'A', 'B', 'C' ]},
-      { number: 2, url: '?page=2', sections: [ 'Y', 'Z' ]},
-    ]
+    beers: [
+      {name: 'Tikibalang Barley Wine', abv: 9.6},
+      {name: 'Hyote Chocolate Stout', abv: 7.4},
+      {name: 'Pope Lick Porter', abv: 6.5},
+      {name: 'Ahool Ale ', abv: 5.4},
+      {name: 'North Adjule Lager', abv: 3.7},
+    ],
+    maxAbv: 7.0
   }
 });
